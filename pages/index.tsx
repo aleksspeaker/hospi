8fc5391433373a53122b79ad26eb2501b7a54dfc
aleksspeaker <aleksspeaker@gmail.com>
2019-02-27 19:47:55 +0200

    Added layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,7 @@
-const IndexPage = () => <h1>Testing Next.js App written in TypeScript with Jest</h1>;
+import Layout from "../src/components/Layout";
+
+const IndexPage = () =>  <Layout title="Home | Next.js + TypeScript Example">
+  <h1>Testing Next.js App written in TypeScript with Jest</h1>
+</Layout>;
 
 export default IndexPage;
