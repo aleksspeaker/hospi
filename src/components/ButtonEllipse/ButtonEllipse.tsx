bdffcae83038215b160ca8e3b2558cd4f97b3ac7
aleksspeaker <aleksspeaker@gmail.com>
2019-03-04 12:26:23 +0200

    Moved buttonEllipse styles to styled-jsx

--- src/components/ButtonEllipse/ButtonEllipse.tsx
+++ src/components/ButtonEllipse/ButtonEllipse.tsx
@@ -1,21 +1,65 @@
 import React from "react";
-// import "./ButtonEllipse.scss";
 
 const ButtonEllipse = (props) => {
    const { text, classes, routeTo } = props;
    return <>
      <a className={"Button-ellipse " + classes} href={routeTo}>{text}</a>
      <style jsx>{`
-        p {
-          color: blue;
+      .Button-ellipse {
+          display: inline-block;
+          border-radius: 18px;
+          font-size: 11px;
+          font-weight: 500;
+          padding: 12px 17px;
+          color: white;
+          text-transform: uppercase;
+          cursor: pointer;
+      }
+
+        .white {
+          background: #fff;
+          color: red;
         }
-        div {
-          background: red;
+
+        .white:hover {
+          background: #d7d7d7;
         }
-        @media (max-width: 600px) {
-          div {
-            background: blue;
-          }
+
+        .orange {
+          background: orange;
+        }
+        .orange:hover {
+          background: #ff8153;
+        }
+
+        .gray {
+          background: #333;
+          font-weight: 600;
+          color: #222;
+        }
+        .gray:hover {
+          background: #c6c7c5;
+        }
+
+        .pale {
+          opacity: .8;
+          background-color: #bc4a24;
+          font-weight: 600;
+          color: white;
+        }
+        .pale:hover {
+          background: #e35a2b;
+        }
+
+        .arrow-white::after {
+          content: url("../../../assets/images/arrow-right-white.svg");
+          margin-left: 25px ;
+        }
+
+
+        .arrow-dark::after {
+          content: url("../../../assets/images/arrow-right-dark.svg");
+          margin-left: 25px;
         }
       `}</style>
     </>;
