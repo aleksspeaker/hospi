d1097d3f55b8d11367c4cdc8c2a46ed61888cb8f
aleksspeaker <aleksspeaker@gmail.com>
2019-03-22 20:52:00 +0200

    added fonts

--- next.config.js
+++ next.config.js
@@ -1,5 +1,19 @@
 const withTypescript = require('@zeit/next-typescript');
 const withSass = require("@zeit/next-sass");
 const withImages = require("next-images");
+const withFonts = require('next-fonts');
 
-module.exports = withTypescript(withSass(withImages()));
+module.exports = withTypescript(withImages(withSass(withFonts({
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.TTF$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 100000
+        }
+      }
+    })
+    return config;
+  }
+}))));
